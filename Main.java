57e6815b1ccd78bbe9adcbb5acfc32abd72eb47c
OlyaSaf <olly.saf@gmail.com>
2022-11-18 16:25:17 +1300

    Reading student list from file

--- Main.java
+++ Main.java
@@ -10,22 +10,7 @@ public class Main {
         private static ArrayList<Student> students = new ArrayList<Student>();
 
         public static void main(String[] args) {
-
-                Student frank = new Student(1, "Frank", "West", new AssignmentMarks("Mathematics", 98, 95, 87),
-                                new AssignmentMarks("English", 78, 77, 88));
-
-                Student dianne = new Student(2, "Dianne", "Greene", new AssignmentMarks("Mathematics", 78, 94, 88),
-                                new AssignmentMarks("English", 87, 95, 92));
-
-                Student doug = new Student(3, "Doug", "Lei", new AssignmentMarks("Mathematics", 78, 94, 88),
-                                new AssignmentMarks("English", 87, 95, 92));
-
-                Student james = new Student(4, "James", "Hewlett", new AssignmentMarks("Mathematics", 69, 92, 74),
-                                new AssignmentMarks("English", 77, 89, 91));
-
-                Student aroha = new Student(5, "Aroha", "Wright", new AssignmentMarks("Mathematics", 97, 92, 87),
-                                new AssignmentMarks("English", 83, 82, 92));
-
+                readFile("Students.txt");
         }
 
         public static boolean readFile(String fileName) {
